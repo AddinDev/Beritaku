c01945a793c18c3df817dfff22c58cc40ac0d1bc
AddinDev <addinsatria2004@gmail.com>
2020-05-10 08:00:55 +0700

    Initial commit

---
+++ app/src/main/java/com/addindev/beritaku/api/ApiInterface.java
@@ -0,0 +1,29 @@
+package com.addindev.beritaku.api;
+
+import com.addindev.beritaku.models.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines")
+    Call<News> getNews(
+
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+
+    );
+
+    @GET("everything")
+    Call<News> getNewsSearch(
+
+            @Query("q") String keyword,
+            @Query("language") String language,
+            @Query("sortBy") String sortBy,
+            @Query("apiKey") String apiKey
+
+    );
+
+}
